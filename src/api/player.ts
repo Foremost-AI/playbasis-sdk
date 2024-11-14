dd9363163d06ccbafa141e7eaba5675fd2e00f4c
Thanakij Pechprasarn <thanakij.p@foremost-ai.com>
2024-11-14 11:59:30 +0700

    feat: add sdk.player.register()

--- src/api/player.ts
+++ src/api/player.ts
@@ -1,5 +1,39 @@
 import { APIClient } from './client';
 
+enum Gender {
+  MALE = 1,
+  FEMALE = 2,
+}
+
+enum ApproveStatus {
+  APPROVED = 'approved',
+  REJECTED = 'rejected',
+  PENDING = 'pending',
+}
+
+interface RegisterRequest {
+  username?: string;
+  password?: string;
+  email?: string;
+  phone_number?: string;
+  first_name?: string;
+  last_name?: string;
+  gender?: Gender;
+  birth_date?: string;
+  code?: string;
+  image?: string;
+  tags?: string;
+  device_id?: string;
+  approve_status?: ApproveStatus;
+}
+
+interface RegisterResponse {
+  success: boolean,
+  error_code: string,
+  message: string,
+  response: any[],
+}
+
 interface PlayerResponse {
   success: boolean;
   error_code: string;
@@ -23,6 +57,14 @@ export class Player {
 
   constructor(apiClient: APIClient) {
     this.apiClient = apiClient;
+  }
+
+  public register(id: string, data?: RegisterRequest): Promise<RegisterResponse> {
+    return this.apiClient.post<RegisterResponse>(`/Player/${id}/register`, {
+      ...data,
+      username: data?.username ?? id,
+      email: data?.email ?? `${id}@email.com`,
+    });
   }
 
   public getPlayer(): Promise<PlayerResponse> {
